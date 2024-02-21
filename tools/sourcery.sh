66a328593af6e4726d98b97d4a28bad31feecc0a
ShlyapkinIN <shlyapkin01@gmail.com>
2024-02-21 19:47:19 +0800

    Update script

--- tools/sourcery.sh
+++ tools/sourcery.sh
@@ -1,3 +1,9 @@
 SOURCERY="sourcery-2.1.3"
-PROJECT_DIR="${WORKSPACE_PATH}/../../.."
+
+if [[ -z "${JENKINS_HOME}" ]]; then
+    PROJECT_DIR="${WORKSPACE_PATH}/../../.."
+else
+    PROJECT_DIR="${WORKSPACE_PATH}/"
+fi
+
 "${PROJECT_DIR}/tools/${SOURCERY}/bin/sourcery" --config "${PROJECT_DIR}/tools/sourcery.yml" --verbose
