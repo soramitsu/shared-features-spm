8fd9491d3ffd61bac1de57af0a682a543a8c2e3d
ShlyapkinIN <shlyapkin01@gmail.com>
2024-03-03 18:21:41 +0800

    Update package

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         .library(name: "SSFUtils", targets: ["SSFUtils"]),
         .library(name: "IrohaCrypto", targets: ["IrohaCrypto"]),
         .library(name: "SSFCloudStorage", targets: ["SSFCloudStorage"]),
+        .library(name: "SSFQRService", targets: ["SSFQRService"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.7"),
