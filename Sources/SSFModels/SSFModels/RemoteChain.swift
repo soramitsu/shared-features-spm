136b821d8afb60b7e484d3e14777d570d9ca4d86
Radmir Dzhurabaev <dzhurabaev@soramitsu.co.jp>
2024-06-13 11:36:07 +0500

    ChainModel equatable fix

--- Sources/SSFModels/SSFModels/RemoteChain.swift
+++ Sources/SSFModels/SSFModels/RemoteChain.swift
@@ -19,10 +19,34 @@ public struct XcmChain: Codable, Equatable {
     }
 
     public static func == (lhs: XcmChain, rhs: XcmChain) -> Bool {
-        lhs.xcmVersion == rhs.xcmVersion &&
-            lhs.availableAssets == rhs.availableAssets &&
-            Set(lhs.availableDestinations) == Set(rhs.availableDestinations) &&
-            lhs.destWeightIsPrimitive ?? false == rhs.destWeightIsPrimitive ?? false
+        let sortedLhsAvailableAssets = lhs.availableAssets.sorted(by: {
+            ($0.id, $0.symbol, $0.minAmount ?? "")
+            >
+            ($1.id, $1.symbol, $1.minAmount ?? "")
+        })
+        let sortedRhsAvailableAssets = rhs.availableAssets.sorted(by: {
+            ($0.id, $0.symbol, $0.minAmount ?? "")
+            >
+            ($1.id, $1.symbol, $1.minAmount ?? "")
+        })
+        let isAvailableAssetsIsEqual = sortedLhsAvailableAssets.elementsEqual(sortedRhsAvailableAssets)
+        
+        let sortedLhsAvailableDest = lhs.availableDestinations.sorted(by: {
+            $0.chainId > $1.chainId
+        })
+        let sortedRhsAvailableDest = rhs.availableDestinations.sorted(by: {
+            $0.chainId > $1.chainId
+        })
+        let isAvailableDestIsEqual = sortedLhsAvailableDest.elementsEqual(sortedRhsAvailableDest)
+        
+        let isEqual = [
+            lhs.xcmVersion == rhs.xcmVersion,
+            lhs.destWeightIsPrimitive ?? false == rhs.destWeightIsPrimitive ?? false,
+            isAvailableAssetsIsEqual,
+            isAvailableDestIsEqual
+        ].allSatisfy { $0 }
+        
+        return isEqual
     }
 }
 
@@ -39,6 +63,28 @@ public struct XcmAvailableDestination: Codable, Hashable {
         self.chainId = chainId
         self.bridgeParachainId = bridgeParachainId
         self.assets = assets
+    }
+    
+    public static func == (lhs: XcmAvailableDestination, rhs: XcmAvailableDestination) -> Bool {
+        let sortedLhsAssets = lhs.assets.sorted(by: {
+            ($0.id, $0.symbol, $0.minAmount ?? "")
+            >
+            ($1.id, $1.symbol, $1.minAmount ?? "")
+        })
+        let sortedRhsAssets = rhs.assets.sorted(by: {
+            ($0.id, $0.symbol, $0.minAmount ?? "")
+            >
+            ($1.id, $1.symbol, $1.minAmount ?? "")
+        })
+        let isAvailableAssetsIsEqual = sortedLhsAssets.elementsEqual(sortedRhsAssets)
+
+        let isEqual = [
+            lhs.chainId == rhs.chainId,
+            lhs.bridgeParachainId == rhs.bridgeParachainId,
+            isAvailableAssetsIsEqual
+        ].allSatisfy { $0 }
+        
+        return isEqual
     }
 }
 
