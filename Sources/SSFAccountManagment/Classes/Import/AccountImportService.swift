aeb62af7a74fe58064a491865f3438742238a86a
ShlyapkinIN <shlyapkin01@gmail.com>
2024-06-21 10:09:25 +0300

    Update

--- Sources/SSFAccountManagment/Classes/Import/AccountImportService.swift
+++ Sources/SSFAccountManagment/Classes/Import/AccountImportService.swift
@@ -66,7 +66,7 @@ extension AccountImportService: AccountImportable {
                 cryptoType: request.cryptoType,
                 defaultChainId: request.defaultChainId
             )
-            
+
             operation = accountOperationFactory.newMetaAccountOperation(
                 mnemonicRequest: request,
                 isBackuped: true
