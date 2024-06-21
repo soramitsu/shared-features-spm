c7aabdd3edf7a5afa01f8226b4e0b1be16e17528
2bogd <99387689+2bogd@users.noreply.github.com>
2024-06-21 09:30:34 +0300

    make ChainAssetsFetchWorkerProtocol public

--- Sources/SSFAssetManagment/AssetFetching/ChainAssetsFetchWorker.swift
+++ Sources/SSFAssetManagment/AssetFetching/ChainAssetsFetchWorker.swift
@@ -4,15 +4,15 @@ import SSFModels
 import SSFUtils
 
 // sourcery: AutoMockable
-protocol ChainAssetsFetchWorkerProtocol {
+public protocol ChainAssetsFetchWorkerProtocol {
     func getChainAssetsModels() async -> [ChainAsset]
 }
 
-final class ChainAssetsFetchWorker: ChainAssetsFetchWorkerProtocol {
+public final class ChainAssetsFetchWorker: ChainAssetsFetchWorkerProtocol {
     private let chainRepository: AnyDataProviderRepository<ChainModel>
     private let operationManager: OperationManagerProtocol
 
-    init(
+    public init(
         chainRepository: AnyDataProviderRepository<ChainModel>,
         operationManager: OperationManagerProtocol = OperationManagerFacade.sharedManager
     ) {
@@ -20,7 +20,7 @@ final class ChainAssetsFetchWorker: ChainAssetsFetchWorkerProtocol {
         self.operationManager = operationManager
     }
 
-    func getChainAssetsModels() async -> [ChainAsset] {
+    public func getChainAssetsModels() async -> [ChainAsset] {
         let operation = chainRepository.fetchAllOperation(with: .none)
         operationManager.enqueue(operations: [operation], in: .transient)
 
