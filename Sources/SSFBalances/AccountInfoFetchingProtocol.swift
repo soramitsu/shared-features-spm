43680db08c4335e3f551ad5250c6515699a0627f
ShlyapkinIN <shlyapkin01@gmail.com>
2024-08-24 18:52:39 +0300

    Update AccountInfoFetching

--- Sources/SSFBalances/AccountInfoFetchingProtocol.swift
+++ Sources/SSFBalances/AccountInfoFetchingProtocol.swift
@@ -5,18 +5,6 @@ import SSFModels
 protocol AccountInfoFetchingProtocol {
     func fetch(
         for chainAsset: ChainAsset,
-        accountId: AccountId,
-        completionBlock: @escaping (ChainAsset, AccountInfo?) -> Void
-    )
-
-    func fetch(
-        for chainAssets: [ChainAsset],
-        accountId: AccountId,
-        completionBlock: @escaping ([ChainAsset: AccountInfo?]) -> Void
-    )
-
-    func fetch(
-        for chainAsset: ChainAsset,
         accountId: AccountId
     ) async throws -> (ChainAsset, AccountInfo?)
 
