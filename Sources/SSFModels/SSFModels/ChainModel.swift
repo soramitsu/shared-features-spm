35c2b5fad855981c21046935cb5957828bc3d5cc
Denis Lebedko <Viscount88@gmail.com>
2024-08-26 13:16:51 +0700

    chain model extension

--- Sources/SSFModels/SSFModels/ChainModel.swift
+++ Sources/SSFModels/SSFModels/ChainModel.swift
@@ -287,6 +287,29 @@ public final class ChainModel: Codable, Identifiable {
             identityChain: identityChain
         )
     }
+    
+    func replacing(_ assets: [AssetModel]) -> ChainModel {
+        ChainModel(
+            rank: rank,
+            disabled: disabled,
+            chainId: chainId,
+            parentId: parentId,
+            paraId: paraId,
+            name: name,
+            assets: Set(assets),
+            xcm: xcm,
+            nodes: nodes,
+            addressPrefix: addressPrefix,
+            types: types,
+            icon: icon,
+            options: options,
+            externalApi: externalApi,
+            selectedNode: selectedNode,
+            customNodes: customNodes,
+            iosMinAppVersion: iosMinAppVersion,
+            identityChain: identityChain
+        )
+    }
 }
 
 public extension ChainModel {
