b8fee3e227daddb6b999031ac9221ee3ad836c58
ShlyapkinIN <shlyapkin01@gmail.com>
2024-08-28 13:36:33 +0300

    Worker default fix

--- Sources/SSFStorageQueryKit/Classes/Async/StorageFactory/StorageFactoryWorkers/JSONRPCWorkerDefault.swift
+++ Sources/SSFStorageQueryKit/Classes/Async/StorageFactory/StorageFactoryWorkers/JSONRPCWorkerDefault.swift
@@ -23,7 +23,7 @@ class JSONRPCWorker<P: Codable, T: Decodable> {
     }
 
     func performCall() async throws -> T {
-        try await withCheckedThrowingContinuation { continuation in
+        try await withUnsafeThrowingContinuation { continuation in
             currentTask = Task {
                 let timeoutTask = Task {
                     let duration = UInt64(timeout * 1_000_000_000)
