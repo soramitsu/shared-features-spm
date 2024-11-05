6a092e75cbb2d91236a453d7cddc07932bc1bfb0
bauyrzhanabdi <bauyrzhan.abdi@nu.edu.kz>
2024-11-05 12:52:33 +0500

    add-favourite-update

--- Sources/SSFAccountManagment/Classes/ManageAccount/AccountManagementService.swift
+++ Sources/SSFAccountManagment/Classes/ManageAccount/AccountManagementService.swift
@@ -17,6 +17,7 @@ public protocol AccountManageble {
     )
     func getAllAccounts() async throws -> [MetaAccountModel]
     func updateEnabilibilty(for chainAssetId: String) async throws -> MetaAccountModel
+    func updateFavourite(for chainId: String) async throws -> MetaAccountModel
     func update(enabledAssetIds: Set<String>) async throws -> MetaAccountModel
     func logout() async throws
 }
@@ -68,6 +69,29 @@ extension AccountManagementService: AccountManageble {
             }
 
             let updatedAccount = wallet.replacing(newEnabledAssetIds: enabledAssetIds)
+
+            selectedWallet.performSave(value: updatedAccount, completionClosure: { _ in
+                continuation.resume(with: .success(updatedAccount))
+            })
+        }
+    }
+
+    public func updateFavourite(for chainId: String) async throws -> MetaAccountModel {
+        try await withCheckedThrowingContinuation { continuation in
+            guard let wallet = selectedWallet.value else {
+                continuation.resume(throwing: AccountManagerServiceError.unexpected)
+                return
+            }
+
+            var favouriteChainIds = wallet.favouriteChainIds
+
+            if let index = favouriteChainIds.firstIndex(of: chainId) {
+                favouriteChainIds.remove(at: index)
+            } else {
+                favouriteChainIds.append(chainId)
+            }
+
+            let updatedAccount = wallet.replacingFavoutites(favouriteChainIds)
 
             selectedWallet.performSave(value: updatedAccount, completionClosure: { _ in
                 continuation.resume(with: .success(updatedAccount))
